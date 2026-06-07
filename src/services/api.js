d06e3f6070d8afc2ae2b03eac986e2176b478c9f
AbdourahmaneDiallo <abdourahmanedi629@gmail.com>
2026-06-07 15:30:10 +1400

    Config Netlify + API URL Railway

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://192.168.1.177:3000/api";
+const API_URL = "https://gestiondepenses-backend-production.up.railway.app/api";
 
 export const login = async (email, password) => {
   const response = await fetch(`${API_URL}/auth/login`, {
